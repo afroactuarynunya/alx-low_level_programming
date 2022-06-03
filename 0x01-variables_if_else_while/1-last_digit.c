2c50f0ff0a3ed498c3150cec6aa43a2c1aa86032
afroactuarynunya <loretta.davordzi@gmail.com>
2022-06-03 00:46:38 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Determines if num is greater than 5, is 0 or is less than 6 and not 0
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, k;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	k = n % 10;
+	if (k > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (k == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}
