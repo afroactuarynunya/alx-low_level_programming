9f18934cacd1ac720ae58f95ffe0e9320cfffccc
afroactuarynunya <loretta.davordzi@gmail.com>
2022-06-03 15:29:58 -0700

    Task 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,36 @@
 */
 int main(void)
 {
-	int 
+	int c, i, j, k;
 
+	for (c = 48; c <= 57; c++)
+		{
+			for (i = 48; i <= 57; i++)
+				{
+					for (j = 48; j <= 57; j++)
+						{
+							for (k = 48; k <= 57; k++)
+								{ 
+									if (((j + k) > (c + i) && j >= c) || c < j)
+										{
+											putchar(c);
+											putchar(i);
+											putchar(' ');
+											putchar(j);
+											putchar(k);
+											if (c + i + j + k == 227 && c == 57)
+												{
+													break;
+												}
+											else
+												{
+													putchar(',');
+													putchar(' ');
+												}
+
+										}
+								}
+						}
+				}
+		}
+}	
